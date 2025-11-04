ee083c2b84aed30de71ae8b063ea451befba0f41
Quang Tran <nhatquang0672@gmail.com>
2025-11-04 15:58:26 +0700

    update for blcx

--- blcxh5/server/start.sh
+++ blcxh5/server/start.sh
@@ -1,5 +1,27 @@
-cd /home/server/blcx-center
-nohup java -jar blcx-center.jar &
 
-cd /home/server/blcx-server
-nohup java -jar blcx-server.jar &
+#!/bin/bash
+set -e
+
+CENTER_PORT=9053
+SERVER_PORT=9056
+
+echo "[INFO] Starting blcx-center on port ${CENTER_PORT}..."
+nohup java -jar /app/blcx-center/blcx-center.jar \
+    > /app/blcx-center/blcx-center.log 2>&1 &
+
+# Wait a few seconds before starting server
+echo "[INFO] Waiting 5 seconds for blcx-center to initialize..."
+sleep 5
+
+echo "[INFO] Starting blcx-server on port ${SERVER_PORT}..."
+nohup java -jar /app/blcx-server/blcx-server.jar \
+    > /app/blcx-server/blcx-server.log 2>&1 &
+
+# Keep container running by following logs
+tail -f /app/blcx-center/blcx-center.log /app/blcx-server/blcx-server.log
+
+# cd /home/server/blcx-center
+# nohup java -jar blcx-center.jar &
+
+# cd /home/server/blcx-server
+# nohup java -jar blcx-server.jar &
